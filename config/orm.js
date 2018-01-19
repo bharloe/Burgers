7a124883627a66b4570a7ecc11e5985ccce07d27
Ben Harloe <ben.harloe@gmail.com>
2018-01-18 18:33:55 -0800

    views working

--- config/orm.js
+++ config/orm.js
@@ -48,7 +48,7 @@ var orm = {
     queryString += "VALUES (";
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
-    console.log(queryString);
+    
     connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
@@ -62,7 +62,7 @@ var orm = {
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
     queryString += condition;
-    console.log(queryString);
+
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
   });
 });
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burger.insertOne([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
